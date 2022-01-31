cbd5f94bbc709bec300f87f0f22934623e9d3a29
reshma-aws <reshua84secret@gmail.com>
2022-01-31 14:14:36 +0530

    Day 1 part 2

---
+++ Day1-1.py
@@ -0,0 +1,11 @@
+# Print the following :
+#======================
+# Day 1 - Python Print Function
+# The function is declared like this:
+# print('what to print')
+
+print('Day 1 - Python Print Function')
+print('The function is declared like this:')
+print("print('what to print')")
+
+# This program makes use of single and double quotes as example to print a string (line 9) which contains python keywords
