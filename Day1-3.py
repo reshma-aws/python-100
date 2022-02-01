220118808e33cccc88c31e9b92e9cae271b48d67
reshma-aws <reshua84secret@gmail.com>
2022-02-01 14:57:57 +0530

    Day 1 part 3

---
+++ Day1-3.py
@@ -0,0 +1,9 @@
+from doctest import OutputChecker
+
+
+print("Hello " + input("What is your name? : " ) + "!")
+
+# Output
+# ======
+# What is your name : <Give the input>
+# Hello <value of input> !
