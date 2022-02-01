a4654c379a77dfc359ac75d7dee07ae43010224e
reshma-aws <reshua84secret@gmail.com>
2022-02-01 16:07:24 +0530

    Day 1 full commit

---
+++ Day1-4.py
@@ -0,0 +1,7 @@
+#Find length of string
+# s = input("What is your name : ? ")
+# print(len(s))
+
+#or 
+
+print( len (input("What is your name : ? ") ) )
---
+++ Day1-5.py
@@ -0,0 +1,11 @@
+a = input("a : ")
+b = input("b : ")
+#Interchange the value of a and b
+
+c = a
+a = b
+b = c
+
+
+print("a : " + a)
+print("b : " + b)
