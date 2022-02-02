7b3bb1f7c666dda2ec2ad042ece00ce27e8e8af8
reshma-aws <reshua84secret@gmail.com>
2022-02-02 15:20:31 +0530

    Day 2 commit

---
+++ Day2-1.py
@@ -0,0 +1,13 @@
+#To show how prints with different data types
+print("Hello")
+
+print (12345) #prints as is 
+
+print (123 + 345) #prints as sum o 123 + 345 = 468
+
+print("123" + "345") #treats the lines as string and concatenates them
+
+print(123_345_567) #the "_" is used to make a large number readable. However the output is "123345567"
+
+street_name = "Abbey Road"
+print(street_name[4] + street_name[7])
